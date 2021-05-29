e33c9ac598143e5438955934bd4750121df2558e
John Wason <wason@wasontech.com>
2021-05-28 23:36:11 -0400

    Fix windows activate script install

--- recipes/tesseract-robotics-superpack/bld.bat
+++ recipes/tesseract-robotics-superpack/bld.bat
@@ -17,6 +17,4 @@ setlocal EnableDelayedExpansion
 for %%F in (activate deactivate) DO (
     if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
     copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
-    :: Copy unix shell activation scripts, needed by Windows Bash users
-    :: copy %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
 )
